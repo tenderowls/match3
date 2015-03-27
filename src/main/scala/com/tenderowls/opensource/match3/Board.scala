086e44d1b6fa290ebccf2f1ffb4ed51f6e98b93c
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2015-03-27 14:01:01 +0300

    Travis CI config

--- src/main/scala/com/tenderowls/opensource/match3/Board.scala
+++ src/main/scala/com/tenderowls/opensource/match3/Board.scala
@@ -227,15 +227,19 @@ object Board {
   implicit class PointMethods(val point:Point) extends AnyVal {
 
     def left = Point(point.x - 1, point.y)
+
     def left(x:Int) = Point(point.x - x, point.y)
 
     def right = Point(point.x + 1, point.y)
+
     def right(x:Int) = Point(point.x + x, point.y)
 
     def top = Point(point.x, point.y - 1)
+
     def top(x:Int) = Point(point.x, point.y - x)
 
     def bottom = Point(point.x, point.y + 1)
+
     def bottom(x:Int) = Point(point.x, point.y + x)
 
     def direction(to:Point) = {
